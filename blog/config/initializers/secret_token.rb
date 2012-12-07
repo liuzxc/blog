0265458e6740eb0f17f6e0fae9b1f714f379f9b6
jason <lxq_102172@163.com>
2012-12-07 21:34:59 +0800

    Add porject blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '526e5df53142ed9ea58424406b095ad3e7ce335e67a105363ce9eb1c9896e081ad4ec795842c1d1afb8aedbac9391e69beb435e89687cf985a66b9ddaa1378ca'
